39db9f5186691685cdae5799491e318e1582b865
EricWRogers <ericwilliamrogers@gmail.com>
2024-09-09 08:29:18 -0500

    Command Pattern

---
+++ Assets/Scripts/GunManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GunManager))]
+public class GunManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GunManager gunManager = (GunManager)target;
+        if (GUILayout.Button("Prev"))
+        {
+            gunManager.PrevGun();
+        }
+
+        if (GUILayout.Button("Next"))
+        {
+            gunManager.NextGun();
+        }
+
+        if (GUILayout.Button("Use"))
+        {
+            gunManager.Use();
+        }
+    }
+}
+
+public class GunManager : MonoBehaviour
+{
+    public List<Weapon> weapons;
+    public Weapon weapon;
+    private int weaponIndex = 0;
+
+    public void Use()
+    {
+        if (weapon)
+            weapon.Use();
+    }
+
+    public void PrevGun()
+    {
+        if (weapons.Count == 0)
+            return;
+        
+        if (weaponIndex == 0)
+            weaponIndex = weapons.Count;
+
+        weaponIndex--;
+        weapon = weapons[weaponIndex];
+    }
+
+    public void NextGun()
+    {
+        if (weapons.Count == 0)
+            return;
+        
+        weaponIndex++;
+        
+        if (weaponIndex == weapons.Count)
+            weaponIndex = 0;
+
+        weapon = weapons[weaponIndex];
+    }
+}
