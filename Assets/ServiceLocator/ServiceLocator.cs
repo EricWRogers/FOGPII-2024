a9ac9775780661a4fe5cf444ff88898f8aa41b35
EricWRogers <ericwilliamrogers@gmail.com>
2024-10-23 07:44:59 -0500

    Service Locator

---
+++ Assets/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour
+{
+    public SLPlayer player;
+    public List<SLEnemey> enemies;
+    public SLAudioManager audioManager;
+}
